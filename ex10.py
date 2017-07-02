3571c00615ada19e058e61b24ed5ac0e418c02bc
Snowball Wang <wjq1996@mail.ustc.edu.cn>
2017-07-03 00:15:47 +0800

    Escape Sequences

---
+++ ex10.py
@@ -0,0 +1,31 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\\\ a \\\\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print tabby_cat
+print persian_cat
+print backslash_cat 
+print fat_cat 
+
+
+# test for this exercise
+
+#while True:
+  #for i in ["/","-","|","\\","|"]:
+     # print "%s\r" % i,
+	  
+# test for extra-ex3
+
+x = "What kind of %s do you want to live?\nGod damned, I don't know!"  % 'life'
+
+print "I wonder, %s." % x
+print "I wonder, %r." % x
+print "I wonder, %r \n." % x
+print "I wonder, \n%r." % x 
