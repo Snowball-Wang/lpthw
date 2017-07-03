97c4e7b26f56effbecd65d0c4321f3c0682b87b7
Snowball Wang <wjq1996@mail.ustc.edu.cn>
2017-07-04 00:27:47 +0800

    Prompting People

---
+++ ex12.py
@@ -0,0 +1,16 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy." % (
+age, height, weight)
+
+
+# this is a test for function input()
+
+age1 = input("How old are you?")
+height1 = input("How tall are you?")
+weight1 = input("How much do you weigh?")
+
+print "again, you're %r old, %r tall and %r heavy." % (
+age1, height1, weight1)
