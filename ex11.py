7dcb8887e3b82abae11b10a213f652917d9c1c05
Snowball Wang <wjq1996@mail.ustc.edu.cn>
2017-07-04 00:18:23 +0800

    input() & raw_input()

---
+++ ex11.py
@@ -0,0 +1,21 @@
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So you're %r old, %r tall and %r heavy." % (age, height, weight)
+x = int(age) + 10
+print "%d." %x
+
+#test for extra-ex1
+
+print "What's your favorate food?"
+food1 = raw_input()
+food2 = input()
+print "How long before you leave school?"
+day1 = raw_input()
+day2 = input()
+
+print "%r, %r, %r." % (day1, day2, day2 * 2)
