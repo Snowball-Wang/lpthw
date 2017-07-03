752e18294c4229fdbc5a04fb71f56da67f2a6acd
Snowball Wang <wjq1996@mail.ustc.edu.cn>
2017-07-04 00:31:28 +0800

    Parameters & Unpacking & Variables

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+
+script, first, second, third = argv 
+
+#script = raw_input("The exercise is NO.")
+#first = raw_input("The first word you type in: ")
+#second = raw_input("The second word you type in: ")
+#third = raw_input("The third word you type in: ")
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third 
